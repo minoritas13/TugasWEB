94e9a4ae002f834d2cf81033bf813fcc1fedd6e9
Nulnul <151645826+HusnulKhotami@users.noreply.github.com>
2025-06-17 22:15:49 +0700

    mengatur root didalam app.php

---
+++ app/core/App.php
@@ -0,0 +1,40 @@
+<?php
+
+class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
+    public function __construct() {
+        $url = $this->parseURL();
+
+        // mengeek apakah $url[0] tersedia
+        if (isset($url[0]) && file_exists('../app/controllers/' . $url[0] . '.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // Cek method
+        if (isset($url[1]) && method_exists($this->controller, $url[1])) {
+            $this->method = $url[1];
+            unset($url[1]);
+        }
+
+        // Sisanya adalah parameter
+        $this->params = $url ? array_values($url) : [];
+
+        // Jalankan controller/method/params
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    }
+
+    public function parseURL() {
+        if (isset($_GET['url'])) {
+            return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
+
+        return [];
+    }
+}
